b13d22f8e4ef7089001b8b2a3c9b653415a2bc8f
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2020-05-04 22:19:11 +1000

    Graphs of Carbon Accretion Rates.

--- 02.Richmond_River_Analysis.R
+++ 02.Richmond_River_Analysis.R
@@ -4,38 +4,62 @@ library(gridExtra)
 library(sjPlot)
 library(sjmisc)
 
+aa <- read.csv("RR_Plant.csv") #Ex aboveground.csv
+bb <- read.csv("RR_Soil.csv") #Ex belowground.csv
+SiteTreat <- read.csv("RR_SiteTreatment.csv")##list sites and corresponding treatments
 
-aa <- read.csv("RR_aboveground.csv")
-bb <- read.csv("RR_belowground.csv")
+#Plant C-Stock model:=========
+Plant_CarbonStock <- select (aa, Site,Treatment2, Total_Aboveground_Biomass_kg_100m2) %>%
+  mutate(Ecosystem = Treatment2,
+         Plant_Biomass = Total_Aboveground_Biomass_kg_100m2 / 10 ) %>% #1 kg/100m2 = 0.1 tonnes/ha
+  na.omit() %>%
+  filter(Ecosystem != "Disturbed") #there are no data for Disturbed sites.
 
-#RR_Model:=========
-#list sites and corresponding treatments
-SiteTreat <- read.csv("RR_SiteTreatment.csv")#to join Treatments to 
+Plant_CarbonStock$Ecosystem <- droplevels(Plant_CarbonStock$Ecosystem)#drop Disturbed level
 
-#Compute Mean corrected C-stock:
-#Corrected C-stock is computed from percentage change in Depth_to after
-#Accounting for compaction usinc Compaction Correction Value.
-CorrectedCarbon <- select(bb, Site_short,Site_Core, Treatment,Depth_to, Depth_Range,
-                               Site_Core, Carbon_stock_in_section_Mg_Cha,
-                               Lab_Compaction_Correction_Value)%>%
-  filter(Treatment != "Saltmarsh_natural") %>%
-  filter(Depth_to <= 100 ) %>%
-  group_by(Site_Core)%>%
-  mutate(TotalPerCore = sum (Carbon_stock_in_section_Mg_Cha)) %>%
-  mutate( DepthCorrected = Depth_to / Lab_Compaction_Correction_Value, #Depth_to = recorded depth of core in the lab
-          DepthChange = DepthCorrected-Depth_to,
-          DepthChangePerc = 100-(DepthChange/Depth_to *100), #Percent increase in depth
-          CorrectedCarbonStock = TotalPerCore * DepthChangePerc/100) %>%#Percent decrease in Carbon Stock when cut at 50cm
-  group_by(Site_Core) %>%  #to sum total Carbon Stock per core.
-  summarise(OC = sum(CorrectedCarbonStock, na.rm = T)) %>%
-  separate(Site_Core,c("Site","Core_Num"), sep = "_") #Separate to have factor for lm
-            
-CorrectedCarbon
+#Run Plant Stock model:
+plant_model <- lm(Plant_Biomass ~ Ecosystem, data = Plant_CarbonStock)
+summary(plant_model )
+tab_model(plant_model)
 
-m <- left_join(CorrectedCarbon,SiteTreat, by = "Site") #join to get Site_Type info for lm model
-m$Treatment2 <- factor(m$Treatment2, levels = c("Disturbed", "Remnant" ,"Active", "Passive" ))
-m$Site_Type <- m$Treatment2
+#Run Plant Stock ANOVA:
+plant_aov <- aov(Plant_Biomass ~ Ecosystem, data = Plant_CarbonStock)
+summary(plant_aov )
+tab_model(plant_aov )
+sjp.aov1 (Plant_CarbonStock$Plant_Biomass,Plant_CarbonStock$Ecosystem,
+          show.summary=T, show.p = T)
 
-model <- lm (OC ~ Site_Type, data = m )
-summary(rr_model)
-tab_model(model)
+#Soil Stock model till 1m deep:====
+#Compute Mean corrected soil C-stock accounting for compaction using Compaction Correction Value.
+NewDATA <- bb
+NewDATA$C_percent <- ifelse(NewDATA$C_perc == 0, 0.001, NewDATA$C_perc)#convert 0 into 0.001 to run log-models if any
+NewDATA$SliceLength.cm <- (NewDATA$Depth_to - NewDATA$Depth_from) #cm
+NewDATA$PipeDiameter.cm <- 5 #Diameter of coring pipes was 5 cm
+NewDATA$SampleVolume.cm3 <- (pi*(NewDATA$PipeDiameter.cm/2)^2)*NewDATA$SliceLength.cm  #slice volume
+NewDATA$dry_bulk_density.gcm3_corrected <- NewDATA$Dry_bulk_density_gcm3 * NewDATA$Lab_Compaction_Correction_Value
+NewDATA$CarbonDensity.gcm3 <- NewDATA$dry_bulk_density.gcm3_corrected * NewDATA$C_percent/100
+NewDATA$CarbonStock.Mgha <- ((NewDATA$CarbonDensity.gcm3  * 100) * NewDATA$SliceLength.cm )
+
+range(NewDATA$Lab_Compaction_Correction_Value, na.rm = T)# Check if all values are  below 1 = If value = 1 this core had no records of compaction
+range(NewDATA$CarbonStock.Mgha, na.rm = T )# 0.0000 125.6347
+range(NewDATA$CarbonDensity.gcm3, na.rm = T )#0.00000000 0.06281735
+
+#Compute total C-stock per core:
+soil <- select(NewDATA, Site, Site_short,Site_Core, Treatment,Depth_to, Depth_Range,
+               Site_Core, Carbon_stock_in_section_Mg_Cha,Treatment2,CarbonStock.Mgha,
+               Lab_Compaction_Correction_Value)%>%
+  filter  (Treatment != "Saltmarsh_natural") %>%
+  filter (Depth_to <= 100 ) %>% #keeping only top 100 cm of each core
+  mutate (Stock = "Soil") %>% #Assigning additional category, indicatig it is soil stock
+  group_by(Site_Core) %>% #grouping by core till 100 cm
+  summarise(TotalCarbonStockPerCore = sum(CarbonStock.Mgha, na.rm = T))%>% #Add-up all slices 
+  separate(Site_Core, into = c("Site","CoreNumber"), sep = "_", remove = F)#Separate Site from core
+
+soil_carbon <- left_join(soil, SiteTreat, by = "Site" )
+soil_carbon$Ecosystem <- soil_carbon$Treatment2
+
+#Run Soil Stock model:
+soil_model <- lm (TotalCarbonStockPerCore ~ Ecosystem, data = soil_carbon )
+summary(soil_model)
+tab_model(soil_model)
+
