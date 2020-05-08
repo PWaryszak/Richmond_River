6de155a55031079bb7c68ca54b313126e885ca37
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2020-05-08 13:37:10 +1000

    Models of stock and carbon in aboveground biomasss added.

--- 02.Richmond_River_Analysis.R
+++ 02.Richmond_River_Analysis.R
@@ -6,30 +6,50 @@ library(sjmisc)
 
 aa <- read.csv("RR_Plant.csv") #Ex aboveground.csv
 bb <- read.csv("RR_Soil.csv") #Ex belowground.csv
-SiteTreat <- read.csv("RR_SiteTreatment.csv")##list sites and corresponding treatments
+SiteTreat <- read.csv("RR_SiteTreatment_PC.csv")##list sites and corresponding treatments
 
-#Plant C-Stock model:=========
+#Plant biomass & carbon model:=========
 Plant_CarbonStock <- select (aa, Site,Treatment2, Total_Aboveground_Biomass_kg_100m2) %>%
   mutate(Ecosystem = Treatment2,
-         Plant_Biomass = Total_Aboveground_Biomass_kg_100m2 / 10 ) %>% #1 kg/100m2 = 0.1 tonnes/ha
+         Total_Aboveground_Biomass_Mg_ha = Total_Aboveground_Biomass_kg_100m2 / 10 , #1 kg/100m2 = 0.1 tonnes/ha
+         Plant_C = 0.464* Total_Aboveground_Biomass_Mg_ha) %>%
   na.omit() %>%
   filter(Ecosystem != "Disturbed") #there are no data for Disturbed sites.
 
 Plant_CarbonStock$Ecosystem <- droplevels(Plant_CarbonStock$Ecosystem)#drop Disturbed level
 
-#Run Plant Stock model:
-plant_model <- lm(Plant_Biomass ~ Ecosystem, data = Plant_CarbonStock)
-summary(plant_model )
-tab_model(plant_model)
+#model Total_Aboveground_Biomass_Mg_ha~Ecosystem:=====
+plant_biomass_model <- lm(Total_Aboveground_Biomass_Mg_ha ~ Ecosystem, data = Plant_CarbonStock)
+summary(plant_biomass_model )
+tab_model(plant_biomass_model)
 
-#Run Plant Stock ANOVA:
-plant_aov <- aov(Plant_Biomass ~ Ecosystem, data = Plant_CarbonStock)
+#model Plant_C~Ecosystem:=====
+plant_carbon_model <- lm(Plant_C ~ Ecosystem, data = Plant_CarbonStock)
+summary(plant_carbon_model )
+tab_model(plant_carbon_model)
+
+#Run Total_Aboveground_Biomass_Mg_ha~Ecosystem ANOVA:
+plant_aov <- aov(Total_Aboveground_Biomass_Mg_ha ~ Ecosystem, data = Plant_CarbonStock)
 summary(plant_aov )
 tab_model(plant_aov )
-sjp.aov1 (Plant_CarbonStock$Plant_Biomass,Plant_CarbonStock$Ecosystem,
+sjp.aov1 (Plant_CarbonStock$Total_Aboveground_Biomass_Mg_ha,Plant_CarbonStock$Ecosystem,
           show.summary=T, show.p = T)
 
-#Soil Stock model till 1m deep:====
+
+#Model Plant_C ~ Stand_Age, with zero intercept:===========
+Plant_CarbonStock2 <-left_join(Plant_CarbonStock, SiteTreat, by = "Site") %>%
+  mutate(Stand_Age = 2017 - as.numeric(as.character(SiteYear))) %>%
+  select(Ecosystem, Plant_C, Stand_Age) 
+
+plantC_model <- lm(Plant_C~ 0+Stand_Age , data = Plant_CarbonStock2)
+summary(plantC_model )
+tab_model(plantC_model)
+
+plot(Plant_C ~ Stand_Age, data = Plant_CarbonStock2, xlim=c(0,100),ylim=c(0,300))
+abline(lm(Plant_C ~ 0+ Stand_Age, data = Plant_CarbonStock2),lwd=4,col='red')
+
+
+#Model Soil Stock ~ Ecosystem till 1m deep:====
 #Compute Mean corrected soil C-stock accounting for compaction using Compaction Correction Value.
 NewDATA <- bb
 NewDATA$C_percent <- ifelse(NewDATA$C_perc == 0, 0.001, NewDATA$C_perc)#convert 0 into 0.001 to run log-models if any
@@ -56,7 +76,7 @@ soil <- select(NewDATA, Site, Site_short,Site_Core, Treatment,Depth_to, Depth_Ra
   separate(Site_Core, into = c("Site","CoreNumber"), sep = "_", remove = F)#Separate Site from core
 
 soil_carbon <- left_join(soil, SiteTreat, by = "Site" )
-soil_carbon$Ecosystem <- soil_carbon$Treatment2
+soil_carbon$Ecosystem <- soil_carbon$SiteRenamed
 
 #Run Soil Stock model:
 soil_model <- lm (TotalCarbonStockPerCore ~ Ecosystem, data = soil_carbon )
